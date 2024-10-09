b8264cf376ecc1a3f43811b01595d97117d031eb
swift1gh <swift1gh@gmail.com>
2024-10-09 18:20:04 +0000

    09-10-24

--- src/components/Selected.jsx
+++ src/components/Selected.jsx
@@ -18,7 +18,8 @@ const Selected = ({ selectedIds }) => {
           </div>
         </div>
       ) : (
-        <div className="flex justify-center items-center">
+        <div className="flex flex-col justify-center items-center gap-5">
+          <h2 className="p-2 font-mono font-medium">Selected Sneakers</h2>
           <div className="gap-y-6 gap-x-[1.25rem] md:gap-10 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 p-2 ">
             {selectedProducts.map((prod) => (
               <Product
@@ -28,6 +29,15 @@ const Selected = ({ selectedIds }) => {
                 Name={prod.name}
               />
             ))}
+          </div>
+
+          <div className="bg-[#b9b8b8] w-4/5 py-4">
+            <h2 className="flex justify-center text-center items-center gap-3 text-xl font-normal font-roboto">
+              Combo Price:{" "}
+              <span className="text-[#9F4B15] font-mono font-bold">
+                GHS 1,700.00
+              </span>
+            </h2>
           </div>
         </div>
       )}
