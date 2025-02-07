20ef0df42ca3e6ee0e08a2ac0daa4edae2f40b4e
swift1gh <swift1gh@gmail.com>
2025-02-07 13:58:36 +0000

    update

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,11 +1,10 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+import bgImage from "../assets/7.jpg";
 
 const MainLayout = () => {
   return (
-    <div
-      className="h-full"
-      style={{ backgroundImage: "url(/src/assets/7.jpg)" }}>
+    <div className="h-full" style={{ backgroundImage: `url(${bgImage})` }}>
       <Outlet />
       {/* The Outlet is a natural component shows what is inside the MainLayout Router in the App.jsx */}
     </div>
