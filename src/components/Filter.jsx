749ffff1723ffb513c79529ba6ee514f6a518472
swift1gh <swift1gh@gmail.com>
2025-02-11 08:12:40 +0000

    filters

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -24,10 +24,12 @@ const Filter = ({ selectedBrand, setSelectedBrand }) => {
     "Christian Dior",
   ];
 
+  const sortedBrands = ["All", ...brands.slice(1).sort()];
+
   return (
     <nav className="px-3 pb-5 bg-[#ffffff00] flex justify-center items-center sticky top-20 w-full z-50">
       <div className="flex gap-2 overflow-x-auto scrollbar-hide p-3 scroll-smooth">
-        {brands.map((brand) => (
+        {sortedBrands.map((brand) => (
           <FilterButton
             key={brand}
             Name={brand}
