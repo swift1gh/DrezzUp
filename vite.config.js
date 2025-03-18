f6ba2095f5bf059fcb250f917d3496c17ac6e6d8
swift1gh <swift1gh@gmail.com>
2025-03-18 13:54:57 +0000

    deployment error

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      external: ["react-chartjs-2", "chart.js"],
+      output: {
+        globals: {
+          "react-chartjs-2": "ReactChartjs2",
+          "chart.js": "Chart",
+        },
+      },
+    },
+  },
+});
